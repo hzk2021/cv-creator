abb33374912a086ed02871ef4552c16d7f0108b3
hzk2021 <hezhenkai1999@gmail.com>
2023-05-17 17:17:35 +0800

    Change component type

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,10 @@
-import React, { ReactDOM } from 'react';
+import React from 'react';
 
-export class Footer extends React.Component {
-    render() {
-        return(
-            <footer className="bg-white p-2 shadow dark:bg-gray-900 sticky top-[100vh] ">
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">hzk2021@GitHub</a>. All Rights Reserved.</span>
-            </footer>
-        );
-    }
+export function Footer() {
+    return(
+        <footer className="bg-white p-2 shadow dark:bg-gray-900 sticky top-[100vh] ">
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">hzk2021@GitHub</a>. All Rights Reserved.</span>
+        </footer>
+    );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React, { ReactDOM } from 'react';
+import React from 'react';
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 
-export class Header extends React.Component {
+export function Header(props) {
 
-    handleDownloadPdf = async () => {
-        const element = this.props.printRef.current;
+    const handleDownloadPdf = async () => {
+        const element = props.printRef.current;
         const canvas = await html2canvas(element);
         const data = canvas.toDataURL('image/png');
 
@@ -18,17 +18,15 @@ export class Header extends React.Component {
         pdf.save('your_cv.pdf');
     }
 
-    render() {
-        return(
+    return(
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
                 <span className="font-semibold text-xl tracking-tight lg:text-5xl md:text-4xl">CV Creator</span>
             </div>
-            <button type="button" className="rounded-none bg-black text-white py-2 px-4" onClick={this.handleDownloadPdf}>Download as PDF</button>
+            <button type="button" className="rounded-none bg-black text-white py-2 px-4" onClick={handleDownloadPdf}>Download as PDF</button>
 
         </nav>
-        );
-    }
+    );
 }
 
